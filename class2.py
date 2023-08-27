f19a35741b5d5961d2311d1ae8a5fd2a16ab796a
ianchristani <ianchristani@gmail.com>
2023-08-27 09:16:33 +0200

    class about primitive types of data

--- class2.py
+++ class2.py
@@ -54,34 +54,3 @@ print(anotherSimpleVariable, type(anotherSimpleVariable))
 var = '3.2'
 var = float(var)
 print(var)
-
-'''
-var = "3.2"
-var = int(var)
-print(var)
-'''
-
-'''
-So far you cold see we are changing the values to be used, by interacting directly with the source code. It is a problem
-since it is not safe for our code and the user might have no knowledge where to change.
-Regarding this, we need another function that allows the user, in a safe way, to insert data into our program.
-Here comes the function input()
-This function has one argument and it is the text that is gonna be shown to the user.
-It is worth to remembering that:
-1. as default, all data provided from input is a STRING, so if you need a different type you need to convert it as describled before.
-2. you need to keep the entered value into a variable, or you will not be able to use it.
-
-let's take a look:
-'''
-
-# using the default type
-anyData = input("Type anything you wan't to: ")
-print(anyData)
-print(type(anyData))
-
-# changing the type to integer (INT)
-yourAge = int(input("How old are you: "))
-print(yourAge)
-print(type(yourAge))
-
-# WARNING: in the last example, the code is waiting a INT number, if you enter any other kind, we will have an ERROR.
